2738703491eec4002134971a7010eba6cdc78549
freddy990117 <freddy990117@gmail.com>
2024-11-08 07:13:35 +0800

    I get that API's Data,But i don't know next step (Get Object)

--- Cursor Project/src/App.jsx
+++ Cursor Project/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
 import Practice1105 from "../../課程練習/Practice1105";
+import HomeWork from "../../課程練習/HomeWork";
 const App = () => {
   return (
     <div>
-      <Practice1105 />
+      <HomeWork />
       {/* <button onClick={}></button> */}
       {/* <h2 className="test">這是一個H2 Tag</h2> */}
     </div>
