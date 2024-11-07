e5a01d665d5e9ec10e79b7eed6d70bacaa66d486
freddy990117 <freddy990117@gmail.com>
2024-11-08 05:49:02 +0800

    Finish 11/08 Practice

--- Cursor Project/src/App.jsx
+++ Cursor Project/src/App.jsx
@@ -1,18 +1,14 @@
-import { Component, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
-import axios from "axios";
 import Practice1105 from "../../課程練習/Practice1105";
-
-function App() {
+const App = () => {
   return (
-    <>
-      <Practice1105 Name="Freddy" Age="25" Where="Taichung" />
+    <div>
+      <Practice1105 />
       {/* <button onClick={}></button> */}
       {/* <h2 className="test">這是一個H2 Tag</h2> */}
-    </>
+    </div>
   );
-}
+};
 
 export default App;
--- 課程練習/Practice1105.jsx
+++ 課程練習/Practice1105.jsx
@@ -1,16 +1,4 @@
-import React from "react";
-import { com1, com2, com3 } from "../Cursor Project/src/test";
-
-const Practice1105 = () => {
-  return (
-    <div>
-      <ComponentProp />
-    </div>
-  );
-};
-
-export default Practice1105;
-
+import React, { useState, useEffect } from "react";
 // 11/05 課程內容 =>
 // npm install [packageName] , Component's Style , Component(prop) ,  State的繼承關係 ,
 // 複數的 Component export 方式 , onClick 點擊事件處理 , useState , useEffect
@@ -60,46 +48,87 @@ const ComponentProp = (prop) => {
           </div>
         );
       })}
-
-      <h3> 我是 {com1}</h3>
     </div>
   );
 };
 
-// 11/05 useState Props  export複數的Components
-// const com1 = () => {
-//   return <p>com1</p>;
-// };
+// 4. Export 複數的 Components
+const Com1 = () => {
+  return (
+    <h1>
+      <Com1 />
+    </h1>
+  );
+};
+const Com2 = () => {
+  return (
+    <h1>
+      <Com2 />
+    </h1>
+  );
+};
+const Com3 = () => {
+  return (
+    <h1>
+      <Com3 />
+    </h1>
+  );
+};
+export { Com1, Com2, Com3 };
+// or export Com1... export Com2... export Com3...
 
-// const com2 = () => {
-//   return <p>com2</p>;
-// };
+//5. UseState 的繼承關係
+const StatePractice = () => {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  return (
+    <div>
+      <h1>This is count : {count1}</h1>
+      <h1>This is count : {count2}</h1>
+      <button
+        onClick={() => {
+          // plus 1
+          setCount1(count1 + 1); // 1.
+          // setCount1((count1) => count1 + 1); // 2.
+          // setCount2(count1 + 1);
+          // 這樣並不會比 count1 多 1，因為 State 的更新是非同步的，結果會在整個 Function 執行完畢後才會刷新
+        }}
+      >
+        Click me
+      </button>
+    </div>
+  );
+};
 
-// const com3 = () => {
-//   return <p>com1</p>;
-// };
+// 6.Effect 應用
+const EffectPractice = () => {
+  const [name, setName] = useState("Freddy is handsome");
+  const changeButton = () => {
+    setName("No ,Freddy not");
+  };
 
-// export { com1, com2 };
+  useEffect(() => {
+    console.log("useEffect is working");
+  }, [name]);
+  // 一開始 render 時會自動執行一次 useEffect，而當 name 被刷新時，useEffect 會在執行一次。
+  // 若沒有 name 只會在一開始 render 時執行。
 
-// const times = (count) => {
-//   return count * 2;
-// };
-// const CountFun = () => {
-//   const [count, setCount] = useState(0);
-//   const [count2, setCount2] = useState(0);
-//   return (
-//     <div>
-//       <h2>Count : {count}</h2>
-//       <h2>Count2 : {count2}</h2>
-//       <button
-//         onClick={() => {
-//           setCount((pre) => pre + 1);
-//           setCount((pre) => pre + 1);
-//           setCount2(times(count * 2));
-//         }}
-//       >
-//         Click me
-//       </button>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h1>{name}</h1>
+      <button onClick={changeButton}>Click me</button>
+    </div>
+  );
+};
+
+const Practice1105 = () => {
+  return (
+    <div>
+      <h1>
+        <EffectPractice />
+      </h1>
+    </div>
+  );
+};
+
+export default Practice1105;
