a49eb22ecc473b2ca2a8fffb1c92109e98acbd24
freddy990117 <freddy990117@gmail.com>
2024-11-14 07:12:59 +0800

    useRef with Life Cycle

--- Cursor Project/src/App.jsx
+++ Cursor Project/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./App.css";
+import Practice1112 from "../../課程練習/Practice1112";
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Practice1112 />
+    </div>
+  );
 };
 
 export default App;
