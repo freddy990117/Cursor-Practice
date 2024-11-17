26e9c3faffb744345806c2192a012e673fd247be
freddy990117 <freddy990117@gmail.com>
2024-11-18 07:14:28 +0800

    差 ref 的綁定練習

--- Practice/Practice1112.jsx
+++ Practice/Practice1112.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 const Practice1112 = () => {
   // 上課內容 :
-  // 生命週期
   // const API = fetch("https://data.moa.gov.tw/api/v1/PesticideCompanyType/");
   // useEffect(() => {
   //   fetch("https://data.moa.gov.tw/api/v1/PesticideCompanyType/").then((res) =>
@@ -9,73 +8,8 @@ const Practice1112 = () => {
   //       setData(data.Data);
   //     })
   //   );
-  //   console.log(data);
-  // }, []);
-  // 我覺得這邊寫錯了
-  // const com2 = ({ Company_Name }) => {
-  //   return <h2>{Company_Name}</h2>;
-  // };
-  // const alertFn = () => {
-  //   alert("Count += 1");
-  // };
-  // const [count, setCount] = useState(0);
-  // useEffect(() => {
-  //   setCount(count + 1);
-  //   document.body.addEventListener("click");
-  // }, []);
-  // const changeCount = () => {
-  //   setCount(count + 1);
-  //   console.log(count);
-  // };
-  // const gloablCount = 0;
-  // const [count, setCount] = useState(0);
-  // const [show, setShow] = useState(false);
-  // const countRef = useRef(0);
-  // const stringRef = useRef();
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setShow(true);
-  //   }, 3000);
-  // }, []);
+
   // 11/14 練習開始 :
-
-  // 2.props 練習
-  const PropsPractice = (props) => {
-    const { name, age, id } = props; //解構 props
-
-    return (
-      <div>
-        <h2>Name : {name}</h2>
-        <h2>age : {age}</h2>
-        <h2>id : {id}</h2>
-      </div>
-    );
-  };
-
-  // 2.5 props + map => props 要在 app.jsx 內在用 props 方式帶入
-  const MapProps = () => {
-    const data = [
-      { name: "alex", gender: "male" },
-      { name: "alex2", gender: "male" },
-      { name: "alex3", gender: "masle" },
-      { name: "alex4", gender: "malse" },
-      { name: "alex5", gender: "maale" },
-      { name: "alex7", gender: "malew" },
-    ];
-    return (
-      <div>
-        {/* 不用先 &&，因為並不是非同步資料 */}
-        {data.map((d, index) => {
-          return (
-            <h2 key={index}>
-              Name:{d.name} <br />
-              Gender:{d.gender}
-            </h2>
-          );
-        })}
-      </div>
-    );
-  };
 
   // 1. 生命週期    1.5 UseEffect 內不能執行重複的 state
   const LfieCycle1 = () => {
@@ -158,26 +92,208 @@ const Practice1112 = () => {
     );
   };
 
-  // 3.useRef
+  // UseEffect 的 return 設定是非常有必要的 !!! 主要用於清除只執行一次的 side effects，不只能夠確保程式的邏輯正確，更能夠提升整體效能
+  // 副作用 (side effects) => 像是 scroll、setTimeout、addEventListener 會依賴、影響 DOM 元素的動作
 
-  return;
-  // <PropsPractice name="Freddy" age="20" id="TW" /> // 2.
-  // <MapProps /> //2.5
+  // 2.props 練習
+  const PropsPractice = (props) => {
+    const { name, age, id } = props; //解構 props
+
+    return (
+      <div>
+        <h2>Name : {name}</h2>
+        <h2>age : {age}</h2>
+        <h2>id : {id}</h2>
+      </div>
+    );
+  };
+
+  // 2.5 props + map => props 要在 app.jsx 內在用 props 方式帶入
+  const MapProps = () => {
+    const data = [
+      { name: "alex", gender: "male" },
+      { name: "alex2", gender: "male" },
+      { name: "alex3", gender: "masle" },
+      { name: "alex4", gender: "malse" },
+      { name: "alex5", gender: "maale" },
+      { name: "alex7", gender: "malew" },
+    ];
+    return (
+      <div>
+        {/* 不用先 &&，因為並不是非同步資料 */}
+        {data.map((d, index) => {
+          return (
+            <h2 key={index}>
+              Name:{d.name} <br />
+              Gender:{d.gender}
+            </h2>
+          );
+        })}
+      </div>
+    );
+  };
+
+  // 3.useRef 的特點 : 當畫面 render 時，不會改變內部的值
+  const UseRef1 = () => {
+    const [number, setNumber] = useState(0);
+    const currentRef = useRef(0);
+    // 當 number 的值改變時，currentRef 也加 1 (但這樣看不出 ref 的作用)
+    useEffect(() => {
+      currentRef.current += 1;
+    }, [number]);
+
+    return (
+      <div>
+        <h2>CurrentRef is : {currentRef.current}</h2>
+        <h2>number is : {number}</h2>
+        <button
+          onClick={() => {
+            setNumber(number + 1);
+          }}
+        >
+          Plus
+        </button>
+      </div>
+    );
+  };
+
+  // 當發生點擊事件時，畫面的值不會改變，但是 useRef 內部的值會改變
+  const UseRef2 = () => {
+    const refNumber = useRef(0);
+    return (
+      <div>
+        <h2>我是 refNumber : {refNumber.current}</h2>
+        <button
+          onClick={() => {
+            refNumber.current += 1;
+            {
+              console.log(refNumber.current);
+            }
+          }}
+        >
+          Click
+        </button>
+      </div>
+    );
+  };
+
+  // ref = {} 的綁定練習
+  const UseRef3 = () => {
+    const initialRef = useRef("I am initialRef");
+    console.log(initialRef.current);
+
+    return (
+      <div>
+        <input type="text" ref={initialRef} />
+        <button
+          onClick={() => {
+            initialRef.current.value = "Change Input Ref by btn";
+          }}
+        >
+          Change
+        </button>
+      </div>
+    );
+    // 但這樣一開始的 input 並沒有顯示 I am initialRef，如何改善 ?
+  };
+
+  // 在 input 內先顯示 ref 的值
+  const UseRef4 = () => {
+    const initialRef2 = useRef(null);
+    // Render 時執行
+    useEffect(() => {
+      // 確定 initialRef2 內沒有值後
+      if (initialRef2.current) {
+        initialRef2.current.value = "I am initialRef2";
+      }
+    }, []);
+
+    return (
+      <div>
+        <input type="text" ref={initialRef2} />
+        <button
+          onClick={() => {
+            initialRef2.current.value = "Change input Ref2 by btn";
+          }}
+        >
+          Change
+        </button>
+      </div>
+    );
+  };
+
+  const UseRef5 = () => {
+    const initialRef5 = useRef("I am initialRef5");
+
+    return (
+      <input
+        ref={(ele) => {
+          initialRef5.current = ele;
+          console.log(ele);
+        }}
+      />
+    );
+  };
+  return (
+    <div>
+      <UseRef5 />
+    </div>
+  );
   // <LfieCycle1/>
   // <LfieCycle2/>
   // <LfieCycle3/>
+  // <MapProps />
+  // <PropsPractice name="Freddy" age="20" id="TW" /> // 2.
+  //<UseRef1>
+  //<UseRef2>
+  // 4.Global vs Local Variable
+  // <div>
+  //   <button
+  //     onClick={() => {
+  //       // 即使 Global Variable + 1，但畫面不 render 的話是沒辦法顯示
+  //       globalConut += 1;
+  //     }}
+  //   >
+  //     globalConut
+  //   </button>
+  //   <GlobalVariable />
+  // </div>;
 };
+// 4.Global vs Local Variable
+// let globalConut = 0;
+// const GlobalVariable = () => {
+//   const [componentCount, setcomponentCount] = useState(0);
+//   // 區域變數設定
+//   const localVariable = 0;
 
+//   // 設定 Component render 時 + 1
+//   useEffect(() => {
+//     localVariable + 1;
+//   }, [componentCount]);
+
+//   return (
+//     <div>
+//       <h2>我是globalConut : {globalConut} </h2>
+//       <h2>我是componentCount : {componentCount} </h2>
+
+//       {/* !!! 但結果並沒有 +1，這是因為區域變數只存在於 Component 內，且每次執行時，都會被重新創建，永遠會保持在當初設定的值*/}
+//       <h2>我是localVariable : {localVariable}</h2>
+
+//       <button
+//         onClick={() => {
+//           setcomponentCount(componentCount + 1);
+//         }}
+//       >
+//         Plus Number
+//       </button>
+//     </div>
+//   );
+// };
 export default Practice1112;
 
-// 1.生命週期     1.5 在 UseEffect 內不能執行重複的 state
-// 2.props 應用  2.5 快速呈現大量的資料 map + component + prop
 // 3.useRef number useRef ref = {} => 指定
-// 4.區域、全域變數
 
-{
-  /* <input type="text" ref={stringRef} /> */
-}
+/* <input type="text" ref={stringRef} /> */
 //     {/* 設定 element 變數讓 ref 綁定 element， */}
 //     {/* <input
 //       type="text"
@@ -192,13 +308,3 @@ export default Practice1112;
 // onClick={() => {
 //   stringRef.current.value = "hi222";
 // stringRef.current.value = "hi";
-// console.log(gloablCount + 1);
-// setCount((count) => count + 1);
-// console.log("count", count);
-// countRef.current = countRef.current + 1;
-// console.log("countRef", countRef);
-// }}
-//     >
-//       Count++
-//     </button> */}
-//   </div>
