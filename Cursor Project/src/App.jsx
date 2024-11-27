9cd1469d97205cdf22862c73714b85ca4a5d19be
freddy990117 <freddy990117@gmail.com>
2024-11-28 05:34:26 +0800

    換成11/26.jsx

--- Cursor Project/src/App.jsx
+++ Cursor Project/src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 import Practice1126 from "../../Practice/Practice1126";
-import Practice1119 from "../../Practice/Practice1119";
 const App = () => {
   return (
     <div>
-      <Practice1119 />
-      {/* <Practice1126 /> */}
+      <Practice1126 />
     </div>
   );
 };
