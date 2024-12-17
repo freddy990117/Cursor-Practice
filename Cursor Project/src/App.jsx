b49b42461b24ba5fae2a78d94a5ea00540960389
freddy990117 <freddy990117@gmail.com>
2024-12-17 18:49:25 +0800

    Practice Router ok!!!

--- Cursor Project/src/App.jsx
+++ Cursor Project/src/App.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import "./App.css";
 import Practice1210 from "../../Practice/Practice1210";
-import Parent from "./Context/Parent";
-import Appppp from "./Context/Appppp";
+import {
+  BrowserRouter,
+  Router,
+  Routes,
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Page404 from "./pages/Page404";
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
 const App = () => {
+  {
+    /* 方法2 */
+  }
+  const router = createBrowserRouter([
+    { path: "/", element: <Homepage /> },
+    { path: "/about", element: <About /> },
+    { path: "*", element: <Page404 /> },
+  ]);
+
   return (
     <div>
-      <Appppp />
+      {/* 方法1 */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="*" element={<Page404 />}></Route>
+        </Routes>
+      </BrowserRouter>
+
+      <RouterProvider router={router} />
     </div>
   );
 };
