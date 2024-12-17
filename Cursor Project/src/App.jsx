8817bd09141ec01ad699666fc85aafbaa74dc744
freddy990117 <freddy990117@gmail.com>
2024-12-17 20:43:01 +0800

    練習Outlet、Link

--- Cursor Project/src/App.jsx
+++ Cursor Project/src/App.jsx
@@ -1,39 +1,11 @@
 import React from "react";
 import "./App.css";
-import Practice1210 from "../../Practice/Practice1210";
-import {
-  BrowserRouter,
-  Router,
-  Routes,
-  Route,
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Page404 from "./pages/Page404";
-import Homepage from "./pages/Homepage";
-import About from "./pages/About";
+import RouterSetup from "./pages/RouterSetup";
+import Practice1217 from "../../Practice/Practice1217";
 const App = () => {
-  {
-    /* 方法2 */
-  }
-  const router = createBrowserRouter([
-    { path: "/", element: <Homepage /> },
-    { path: "/about", element: <About /> },
-    { path: "*", element: <Page404 /> },
-  ]);
-
   return (
     <div>
-      {/* 方法1 */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="*" element={<Page404 />}></Route>
-        </Routes>
-      </BrowserRouter>
-
-      <RouterProvider router={router} />
+      <RouterSetup />
     </div>
   );
 };
--- Cursor Project/src/pages/Homepage.jsx
+++ Cursor Project/src/pages/Homepage.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const Homepage = () => {
-  return <div>Homepage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h2>This is HomePage : )</h2>
+      <button
+        onClick={() => {
+          navigate("/book", { state: { bookName: "大人學選擇" } });
+        }}
+      >
+        大人學選擇
+      </button>
+      <button
+        onClick={() => {
+          navigate("/book", { state: { bookName: "大人學做事做人" } });
+        }}
+      >
+        大人學做事做人
+      </button>
+      <button
+        onClick={() => {
+          navigate("/book", { state: { bookName: "12周做完一年工作" } });
+        }}
+      >
+        12周做完一年工作
+      </button>
+    </div>
+  );
 };
 
 export default Homepage;
